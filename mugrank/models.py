75964962e562c6b6ae1980240fbf33a6b2e7b0cb
Joe Marriage <josephmarriage@gmail.com>
2023-04-04 16:54:21 +0100

    Add showStats field to list

--- mugrank/models.py
+++ mugrank/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class List(models.Model):
     name = models.CharField(max_length=100)
+    showStats = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
