379f6c73df46d6eec88794e8353fda6b1051c917
physi3 <josephmarriage@gmail.com>
2023-10-07 14:48:49 +0100

    Update shell script, fix bug

--- runserver.sh
+++ runserver.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
 cd /home/pi/Web/Joe-Web
-tmux new -A -s JoeWeb 'python3 manage.py runserver 0.0.0.0:8000 --insecure'
+echo "Opening TMUX session"
+/usr/bin/tmux new-session -d -s JoeWeb '/home/pi/Downloads/Python-3.9.6/python /home/pi/Web/Joe-Web/manage.py runserver 0.0.0.0:8000 --insecure'
