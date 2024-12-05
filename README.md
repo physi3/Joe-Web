e256bdd7339afb9163fe135cef4355906dc9057d
Joe <joe@miniserver.home>
2024-12-05 16:52:49 +0000

    Create simpler environment setup.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Joe Web
  
+## Setting Up Environment Variables
+1. Copy `.env.template` to `.env`:
+   ```bash
+   cp .env.template .env
+   ```
--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/pi/Web/Joe-Web
+cd /home/joe/Web/Joe-Web
 echo "Opening TMUX session"
-/usr/bin/tmux new-session -d -s JoeWeb '/home/pi/Downloads/Python-3.9.6/python /home/pi/Web/Joe-Web/manage.py runserver 0.0.0.0:8000 --insecure'
+/usr/bin/tmux new-session -d -s JoeWeb '/home/joe/Web/Joe-Web/Joe-Web/bin/python /home/joe/Web/Joe-Web/manage.py runserver 0.0.0.0:8000 --insecure'
