7240628296896daf053f0278c55341f1fccf8d80
Joe Marriage <45903919+physi3@users.noreply.github.com>
2024-12-28 15:31:45 +0000

    Switch to Gunicorn and fix bugs (#42)
    * Switch to Gunicorn (#41)
    
    * Create dev.py
    
    * Update requirements.txt
    
    * Collect static etc
    
    * Cleanup
    
    * Revert static changes
    
    * Fix static files
    
    ---------
    
    Co-authored-by: Joe <joe@miniserver.home>
    
    * Fix mugrank favicon issue
    
    * Update requirements.txt
    
    ---------
    
    Co-authored-by: Joe <joe@miniserver.home>

--- runserver.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/joe/Web/Joe-Web
-echo "Opening TMUX session"
-/usr/bin/tmux new-session -d -s JoeWeb '/home/joe/Web/Joe-Web/Joe-Web/bin/python /home/joe/Web/Joe-Web/manage.py runserver 0.0.0.0:8000 --insecure'
