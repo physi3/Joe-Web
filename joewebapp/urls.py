ec5175ce415a31c07960d790dca0b3d24a851997
Joe Marriage <josephmarriage@gmail.com>
2025-02-07 18:49:56 +0000

    Create service authorisation backend

--- joewebapp/urls.py
+++ joewebapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
+
 urlpatterns = [
     path('', views.index),
     path('sketches/', views.sketches),
     path('sketches/<str:sketch>/', views.sketchView),
     path('sketches/<str:sketch>/source/', views.sourceView),
+    path("login/", auth_views.LoginView.as_view(template_name="auth/login.html"), name="login"),
+    path("auth/<str:service>/", views.auth),
 ]
--- joewebapp/views.py
+++ joewebapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.templatetags.static import static
 from json import loads as loadJson
+from .models import *
 
 def index(request):
     return render(request, "index.html")
@@ -40,3 +41,23 @@ def sourceView(request, sketch):
     ctx = {"sketch":sketchObj, "source":source}
 
     return render(request, f"sketches/source.html", ctx)
+
+def auth(request, service):
+    body = {}
+
+    if request.user.is_authenticated:
+        user = request.user
+        body["username"] = user.username
+    else:
+        return JsonResponse({"error": "Not logged in"}, status=401)
+
+    try:
+        serviceObject = Service.objects.get(name__iexact=service)
+        body["service"] = serviceObject.name
+        
+    except Service.DoesNotExist:
+        return JsonResponse({"error": "Invalid service"}, status=400)
+
+    body["authorized"] = user.is_superuser or UserServiceAccess.objects.filter(user=request.user, service=serviceObject).exists()
+    statusCode = 200 if body["authorized"] else 403
+    return JsonResponse(body, status = statusCode)
