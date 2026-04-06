cae4557230b998b43748bb7e0c56923b5b13a395
Joe Marriage <45903919+physi3@users.noreply.github.com>
2026-04-06 16:41:07 +0100

    Add basic auth checking
    * Create service authorisation backend
    
    * Remove auth host
    
    * Create custom login view

--- joewebapp/models.py
+++ joewebapp/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Service(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class UserServiceAccess(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("user", "service")  # Prevent duplicates
+
+    def __str__(self):
+        return f"{self.user.username} -> {self.service.name}"
--- joewebapp/urls.py
+++ joewebapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.index),
     path('sketches/', views.sketches),
     path('sketches/<str:sketch>/', views.sketchView),
     path('sketches/<str:sketch>/source/', views.sourceView),
+    path("login/", views.JoeLoginView.as_view(template_name="auth/login.html", next_page="/"), name="login"),
+    path("auth/<str:service>/", views.auth),
 ]
